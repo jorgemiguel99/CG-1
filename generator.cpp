b7f5e9787522f5f1f5d963ed71789d3573ef7568
zecarlos94 <zecarlos_94@hotmail.com>
2016-02-21 13:10:23 +0000

    CG-2
    melhorei tudo para ficar igual ao enunciado

--- generator.cpp
+++ generator.cpp
@@ -6,29 +6,37 @@
 #include <sstream>
 using namespace std;
 
+string filename, fileExtension = ".3d";
+
 //operation name has 100 char at max
 #define MAX 100
 
 //loads the coordenates to make a box
 void box(float X, float Y, float Z) {
-	cout << "This is the Drawing of a Box!!!" << endl;
+	cout << "This is the Drawing of a Box with " << X << ", " << Y << " and " << Z << " as inputs in file: " << filename << endl;
 }
 
 //loads the coordenates to make a plane with square == 2 triangles => 4 points => 8 coordenates X,Y
 void plane(float Xa, float Ya, float Xb, float Yb, float Xc, float Yc, float Xd, float Yd) {
 	//put the formulas to draw the plane
-	cout << "This is the Drawing of a Plane!!!" << endl;
+	cout << "This is the Drawing of a Plane with "
+		<< "(" << Xa << "," << Ya << ")" << ";"
+		<< "(" << Xb << "," << Yb << ")" << ";"
+		<< "(" << Xc << "," << Yc << ")" << " & "
+		<< "(" << Xd << "," << Yd << ")" << "in file: "
+		<< filename
+		<< endl;
 }
 
 //loads the inputs in order to produce a sphere
 void sphere(float radiusV, float sliceV, float stackV) {
 	//put the formulas to draw the sphere
-	cout << "This is the Drawing of a Sphere!!!" << endl;
+	cout << "This is the Drawing of a Sphere with " << radiusV << ", " << sliceV << " and " << stackV << " as inputs in file: " << filename << endl;
 }
 
 //loads the inputs in order to produce a cone
 void cone(float bottomRadius, float heightC, float slicesC, float stacksC) {
-	cout << "This is the Drawing of a Cone!!!" << endl;
+	cout << "This is the Drawing of a Cone with " << bottomRadius << ", " << heightC << ", " << slicesC << " and " << stacksC << " as inputs in file: " << filename << endl;
 }
 
 //Return the number to be used on the switch in main function; splitted[1]<=>operation name 
@@ -55,8 +63,8 @@ vector<string> split(string str, char delimiter) {
 
 int main(int argc, char **argv) {
 
-	string operationLine, operation, filename, fileExtension;
-	fileExtension = ".3d";
+	string operationLine, operation;
+
 	cout << "Insert your operation:" << endl;
 	getline(cin, operationLine);
 	//guarda a string no vector com as substrings separadas por um espaço
@@ -74,32 +82,33 @@ int main(int argc, char **argv) {
 	 */
 
 	//stof is a function that transforms the content of string into float
-	 
-	switch (checkOP(splitted[0])) {
+	if (splitted[0] == "Generator" || splitted[0] == "generator") {
+		switch (checkOP(splitted[1])) {
 
-	case 1:		  if (splitted.size() == 10) {
-					plane(stof(splitted[1]), stof(splitted[2]), stof(splitted[3]), stof(splitted[4]), stof(splitted[5]), stof(splitted[6]), stof(splitted[7]), stof(splitted[8]));
-					filename = splitted[9] + fileExtension;
-				  }
-				  break;
-	case 2:       if (splitted.size() == 5) {
-					box(stof(splitted[1]), stof(splitted[2]), stof(splitted[3]));
-					filename = splitted[4] + fileExtension;
-				  }
-				  break;
-	case 3:       if (splitted.size() == 5) {
-					sphere(stof(splitted[1]), stof(splitted[2]), stof(splitted[3]));
-					filename = splitted[4] + fileExtension;
-				  }
-				  break;
-	case 4:       if (splitted.size() == 6) {
-					cone(stof(splitted[1]), stof(splitted[2]), stof(splitted[3]), stof(splitted[4]));
-					filename = splitted[5] + fileExtension;
-				  }
-				  break;
-	default:      cout << "Not an operation!\n" << endl;
-				  filename = "invalid.3d";
-				  break;
+		case 1:		  if (splitted.size() == 11) {
+			                filename = splitted[10] + fileExtension;
+							plane(stof(splitted[2]), stof(splitted[3]), stof(splitted[4]), stof(splitted[5]), stof(splitted[6]), stof(splitted[7]), stof(splitted[8]), stof(splitted[9]));
+					  }
+					  break;
+		case 2:       if (splitted.size() == 6) {
+			                filename = splitted[5] + fileExtension;
+							box(stof(splitted[2]), stof(splitted[3]), stof(splitted[4]));
+					  }
+					  break;
+		case 3:       if (splitted.size() == 6) {
+			                filename = splitted[5] + fileExtension;
+							sphere(stof(splitted[2]), stof(splitted[3]), stof(splitted[4]));
+					  }
+					  break;
+		case 4:       if (splitted.size() == 7) {
+			                filename = splitted[6] + fileExtension;
+							cone(stof(splitted[2]), stof(splitted[3]), stof(splitted[4]), stof(splitted[5]));
+					  }
+					  break;
+		default:      cout << "Not an operation!\n" << endl;
+					  filename = "invalid.3d";
+					  break;
+		}
 	}
 	return 1;
 }
