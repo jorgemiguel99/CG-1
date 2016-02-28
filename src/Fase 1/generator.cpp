d4b0a342dfea134030036f4f0a23249329cdf7f6
tigas95 <tiago.carvalhais.1995@gmail.com>
2016-02-28 00:52:37 +0000

    dei uns retoques ao codigo

--- src/Fase 1/generator.cpp
+++ src/Fase 1/generator.cpp
@@ -7,38 +7,32 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <GL/glut.h>
+#include <math.h>
 //#include <GLUT/glut.h> //-- MAC
 
 #define _USE_MATH_DEFINES
-#include <math.h>
 
 using namespace std;
 
-//Global Variables Handling Files
+// Global variables handling files
 string desktop = "C:\\Users\\Tiago\\Desktop\\Universidade\\3º Ano\\CG\\Projeto\\Fase 1\\Ficheiros 3d";
 //string desktop = "/Users/zecarlos/Desktop/";// -- MAC
-string filename, fileExtension = ".3d";
+string filename;
 
-//Global Variables Glut functions
-float dimension; //Box
-float radius; //Sphere
-float baseRadius; //Cone
-int slices, stacks; //Sphere & Cone
+// Dimensions of the figures
+float length, width, height, radius, slices, stacks;
 
-// Dimens�es do plano e da caixa
-float comprimento, largura, altura;
-
-// Vari�veis usadas para mover a c�mara
+// Variables used to move the camera 
 float moveX = 0.0f, moveY = 0.0f, moveZ = 0.0f, rotateX = 0.0f, rotateY = 0.0f, rotateZ = 0.0f, angle = 0;
 
-//Global Variable Process Input
+// Global variable process input
 vector<string> splitted;
 
-//operation name has 100 char at max
+// Operation name has 100 char at max
 #define MAX 100
 
-//Return the number to be used on the switch in main function; splitted[1]<=>operation name
-int checkOP(string str){
+// Return the number to be used on the switch in main function; splitted[1] = operation name
+int checkOP(string str) {
     if (str == "Plane" || str == "plane") return 1;
     else if (str == "Box" || str == "box") return 2;
     else if (str == "Sphere" || str == "sphere") return 3;
@@ -46,7 +40,7 @@ int checkOP(string str){
     else return -1;
 }
 
-//function that splits a string into its substrings accordingly to its delimiter that must be provided
+// Splits a string into its substrings accordingly to its delimiter that must be provided
 vector<string> split(string str, char delimiter) {
     vector<string> internal;
     stringstream ss(str); // Turn the string into a stream.
@@ -63,76 +57,76 @@ void print3d(string figure) {
 	if (figure == "plane") {
 		ofstream plane;
 		plane.open(filename.c_str());
-		plane << "6" << endl; // total number of vertex
+		plane << "6" << endl; // Total number of vertex
 
 		plane << "0.0f, 0.0f, 0.0f" << endl;
-		plane << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
-		plane << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
+		plane << length << ".0f" << ", 0.0f, 0.0f" << endl;
+		plane << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
 
-		plane << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
-		plane << comprimento << ".0f" << ", " << largura << ".0f" << ", 0.0f" << endl;
-		plane << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
+		plane << length << ".0f" << ", 0.0f, 0.0f" << endl;
+		plane << length << ".0f" << ", " << width << ".0f" << ", 0.0f" << endl;
+		plane << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
 
 		plane.close();
 	}
 	else if (figure == "box") {
 		ofstream box;
 		box.open(filename.c_str());
-		box << "36" << endl; // total number of vertex
+		box << "36" << endl; // Total number of vertex
 
-		// Face Inferior
-		box << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
+		// Lower face
+		box << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
+		box << length << ".0f" << ", 0.0f, 0.0f" << endl;
 		box << "0.0f, 0.0f, 0.0f" << endl;
 
-		box << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
-		box << comprimento << ".0f" << ", " << largura << ".0f" << ", 0.0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
+		box << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
+		box << length << ".0f" << ", " << width << ".0f" << ", 0.0f" << endl;
+		box << length << ".0f" << ", 0.0f, 0.0f" << endl;
 
-		// Face Superior
-		box << "0.0f, 0.0f, " << altura << ".0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, " << altura << ".0f" << endl;
-		box << "0.0f, " << largura << ".0f, " << altura << ".0f" << endl;
+		// Upper face
+		box << "0.0f, 0.0f, " << height << ".0f" << endl;
+		box << length << ".0f" << ", 0.0f, " << height << ".0f" << endl;
+		box << "0.0f, " << width << ".0f, " << height << ".0f" << endl;
 
-		box << comprimento << ".0f" << ", 0.0f, " << altura << ".0f" << endl;
-		box << comprimento << ".0f, " << largura << ".0f, " << altura << ".0f" << endl;
-		box << "0.0f, " << largura << ".0f, " << altura << ".0f" << endl;
+		box << length << ".0f" << ", 0.0f, " << height << ".0f" << endl;
+		box << length << ".0f, " << width << ".0f, " << height << ".0f" << endl;
+		box << "0.0f, " << width << ".0f, " << height << ".0f" << endl;
 
-		// Face Direita
-		box << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
-		box << comprimento << ".0f" << ", " << largura << ".0f" << ", 0.0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, " << altura << ".0f" << endl;
+		// Right face
+		box << length << ".0f" << ", 0.0f, 0.0f" << endl;
+		box << length << ".0f" << ", " << width << ".0f" << ", 0.0f" << endl;
+		box << length << ".0f" << ", 0.0f, " << height << ".0f" << endl;
 
-		box << comprimento << ".0f, " << largura << ".0f, 0.0f" << endl;
-		box << comprimento << ".0f, " << largura << ".0f, " << altura << ".0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, " << altura << ".0f" << endl;
+		box << length << ".0f, " << width << ".0f, 0.0f" << endl;
+		box << length << ".0f, " << width << ".0f, " << height << ".0f" << endl;
+		box << length << ".0f" << ", 0.0f, " << height << ".0f" << endl;
 
-		// Face Esquerda
-		box << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
-		box << "0.0f, 0.0f, " << altura << ".0f" << endl;
-		box << "0.0f, " << largura << ".0f, " << altura << ".0f" << endl;
+		// Left Face
+		box << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
+		box << "0.0f, 0.0f, " << height << ".0f" << endl;
+		box << "0.0f, " << width << ".0f, " << height << ".0f" << endl;
 
 		box << "0.0f, 0.0f, 0.0f" << endl;
-		box << "0.0f, 0.0f, " << altura << ".0f" << endl;
-		box << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
+		box << "0.0f, 0.0f, " << height << ".0f" << endl;
+		box << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
 
-		// Face da Frente
+		// Front face
 		box << "0.0f, 0.0f, 0.0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
-		box << "0.0f, 0.0f, " << altura << ".0f" << endl;
+		box << length << ".0f" << ", 0.0f, 0.0f" << endl;
+		box << "0.0f, 0.0f, " << height << ".0f" << endl;
 
-		box << comprimento << ".0f" << ", 0.0f, 0.0f" << endl;
-		box << comprimento << ".0f" << ", 0.0f, " << altura << ".0f" << endl;
-		box << "0.0f, 0.0f, " << altura << ".0f" << endl;
+		box << length << ".0f" << ", 0.0f, 0.0f" << endl;
+		box << length << ".0f" << ", 0.0f, " << height << ".0f" << endl;
+		box << "0.0f, 0.0f, " << height << ".0f" << endl;
 
-		// Face de Tras
-		box << "0.0f, " << largura << ".0f" << ", 0.0f" << endl;
-		box << "0.0f, " << largura << ".0f, " << altura << ".0f" << endl;
-		box << comprimento << ".0f, " << largura << ".0f, 0.0f" << endl;
+		// Back face
+		box << "0.0f, " << width << ".0f" << ", 0.0f" << endl;
+		box << "0.0f, " << width << ".0f, " << height << ".0f" << endl;
+		box << length << ".0f, " << width << ".0f, 0.0f" << endl;
 
-		box << comprimento << ".0f, " << largura << ".0f, 0.0f" << endl;
-		box << "0.0f, " << largura << ".0f, " << altura << ".0f" << endl;
-		box << comprimento << ".0f, " << largura << ".0f, " << altura << ".0f" << endl;
+		box << length << ".0f, " << width << ".0f, 0.0f" << endl;
+		box << "0.0f, " << width << ".0f, " << height << ".0f" << endl;
+		box << length << ".0f, " << width << ".0f, " << height << ".0f" << endl;
 
 		box.close();
 	}
@@ -140,10 +134,9 @@ void print3d(string figure) {
 
 void changeSize(int w, int h) {
     // Prevent a divide by zero, when window is too short
-    // (you cant make a window with zero width).
     if (h == 0) h = 1;
 
-    // compute window's aspect ratio
+    // Compute window's aspect ratio
     float ratio = w * 1.0 / h;
 
     // Set the projection matrix as current
@@ -158,12 +151,12 @@ void changeSize(int w, int h) {
     // Set perspective
     gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
 
-    // return to the model view matrix mode
+    // Return to the model view matrix mode
     glMatrixMode(GL_MODELVIEW);
 }
 
 void renderScene(void) {
-    // Clear Buffers
+    // Clear buffers
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Set the Camera
@@ -180,13 +173,13 @@ void renderScene(void) {
 
 		glColor3f(0, 1, 0);
 		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(comprimento, 0.0f, 0.0f);
-		glVertex3f(0.0f, largura, 0.0f);
+		glVertex3f(length, 0.0f, 0.0f);
+		glVertex3f(0.0f, width, 0.0f);
 
 		glColor3f(0, 1, 0);
-		glVertex3f(comprimento, 0.0f, 0.0f);
-		glVertex3f(comprimento, largura, 0.0f);
-		glVertex3f(0.0f, largura, 0.0f);
+		glVertex3f(length, 0.0f, 0.0f);
+		glVertex3f(length, width, 0.0f);
+		glVertex3f(0.0f, width, 0.0f);
 
 		glEnd();
 
@@ -196,71 +189,71 @@ void renderScene(void) {
     else if(checkOP(splitted[1])==2) {
 		glBegin(GL_TRIANGLES);
 
-		// Face Inferior
+		// Lower face
 		glColor3f(1, 0, 0);
-		glVertex3f(0.0f, largura, 0.0f);
-		glVertex3f(comprimento, 0.0f, 0.0f);
+		glVertex3f(0.0f, width, 0.0f);
+		glVertex3f(length, 0.0f, 0.0f);
 		glVertex3f(0.0f, 0.0f, 0.0f);
 
 		glColor3f(1, 0, 0);
-		glVertex3f(0.0f, largura, 0.0f);
-		glVertex3f(comprimento, largura, 0.0f);
-		glVertex3f(comprimento, 0.0f, 0.0f);
+		glVertex3f(0.0f, width, 0.0f);
+		glVertex3f(length, width, 0.0f);
+		glVertex3f(length, 0.0f, 0.0f);
 
-		// Face Superior
+		// Upper face
 		glColor3f(1, 0, 0);
-		glVertex3f(0.0f, 0.0f, altura);
-		glVertex3f(comprimento, 0.0f, altura);
-		glVertex3f(0.0f, largura, altura);
+		glVertex3f(0.0f, 0.0f, height);
+		glVertex3f(length, 0.0f, height);
+		glVertex3f(0.0f, width, height);
 
 		glColor3f(1, 0, 0);
-		glVertex3f(comprimento, 0.0f, altura);
-		glVertex3f(comprimento, largura, altura);
-		glVertex3f(0.0f, largura, altura);
+		glVertex3f(length, 0.0f, height);
+		glVertex3f(length, width, height);
+		glVertex3f(0.0f, width, height);
 
-		// Face Direita
+		// Right face
 		glColor3f(0, 1, 0);
-		glVertex3f(comprimento, 0.0f, 0.0f);
-		glVertex3f(comprimento, largura, 0.0f);
-		glVertex3f(comprimento, 0.0f, altura);
+		glVertex3f(length, 0.0f, 0.0f);
+		glVertex3f(length, width, 0.0f);
+		glVertex3f(length, 0.0f, height);
 
 		glColor3f(0, 1, 0);
-		glVertex3f(comprimento, largura, 0.0f);
-		glVertex3f(comprimento, largura, altura);
-		glVertex3f(comprimento, 0.0f, altura);
+		glVertex3f(length, width, 0.0f);
+		glVertex3f(length, width, height);
+		glVertex3f(length, 0.0f, height);
 
-		// Face Esquerda
+		// Left face
 		glColor3f(0, 1, 0);
-		glVertex3f(0.0f, largura, 0.0f);
-		glVertex3f(0.0f, 0.0f, altura);
-		glVertex3f(0.0f, largura, altura);
+		glVertex3f(0.0f, width, 0.0f);
+		glVertex3f(0.0f, 0.0f, height);
+		glVertex3f(0.0f, width, height);
 
 		glColor3f(0, 1, 0);
 		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, altura);
-		glVertex3f(0.0f, largura, 0.0f);
+		glVertex3f(0.0f, 0.0f, height);
+		glVertex3f(0.0f, width, 0.0f);
 
-		// Face da Frente
+		// Front face
 		glColor3f(0, 0, 1);
 		glVertex3f(0.0f, 0.0f, 0.0f);
-		glVertex3f(comprimento, 0.0f, 0.0f);
-		glVertex3f(0.0f, 0.0f, altura);
+		glVertex3f(length, 0.0f, 0.0f);
+		glVertex3f(0.0f, 0.0f, height);
 
 		glColor3f(0, 0, 1);
-		glVertex3f(comprimento, 0.0f, 0.0f);
-		glVertex3f(comprimento, 0.0f, altura);
-		glVertex3f(0.0f, 0.0f, altura);
+		glVertex3f(length, 0.0f, 0.0f);
+		glVertex3f(length, 0.0f, height);
+		glVertex3f(0.0f, 0.0f, height);
 
-		// Face de Tr�s
+		// Back face
 		glColor3f(0, 0, 1);
-		glVertex3f(0.0f, largura, 0.0f);
-		glVertex3f(0.0f, largura, altura);
-		glVertex3f(comprimento, largura, 0.0f);
+		glVertex3f(0.0f, width, 0.0f);
+		glVertex3f(0.0f, width, height);
+		glVertex3f(length, width, 0.0f);
 
 		glColor3f(0, 0, 1);
-		glVertex3f(comprimento, largura, 0.0f);
-		glVertex3f(0.0f, largura, altura);
-		glVertex3f(comprimento, largura, altura);
+		glVertex3f(length, width, 0.0f);
+		glVertex3f(0.0f, width, height);
+		glVertex3f(length, width, height);
 
 		glEnd();
 
@@ -268,23 +261,10 @@ void renderScene(void) {
     }
 	// Sphere
     else if(checkOP(splitted[1])==3) {
-        glutWireSphere(radius, slices, stacks);
-        //enable growing effect
-        if (radius < 1.5 | slices < 15 | stacks < 30) {
-            radius += 0.01;
-            slices += 1;
-            stacks += 1;
-        }
+        // Under construction...
     }
     else if(checkOP(splitted[1])==4) { //Cone
-		glutWireCone(baseRadius, altura, slices, stacks);
-         //enable growing effect
-		if (baseRadius < 1.5 | altura < 1.5 | slices < 15 | stacks < 30) {
-			baseRadius += 0.01;
-			altura += 0.01;
-			slices += 1;
-			stacks += 1;
-		}
+		// Under construction...
 	}
 
     // End of frame
@@ -312,71 +292,57 @@ void rotation(unsigned char key, int x, int y) {
 		case 's': angle += 2; rotateX = 1; rotateZ = 0; rotateY = 0; break;
 		case 'e': angle += 2; rotateY = 1; rotateZ = 0; rotateX = 0; break;
 		case 'q': angle -= 2; rotateY = 1; rotateZ = 0; rotateX = 0; break;
-		case 27 : exit(-1); //ESC -> leaves
+		case 27 : exit(-1); // Esc -> leaves
 	}
 	glutPostRedisplay();
 }
 
-// Create filename.3d in Desktop and in .exe folder
+// Create filename.3d in desktop and in .exe folder
 void create3dFile(string fileNm) {
 	ofstream File(filename);
 	ofstream desktopFile(desktop + filename);
 }
 
-// Write xml into a .xml file created before
-void writeXMLFile(string fileNm) {
-	string aux = "generator.xml";
-	ofstream fileXML;
-	fileXML.open(aux.c_str());
-	fileXML << "<scene>\n\t<model file = �" << fileNm << "� />\n</scene>" << endl;
-	fileXML.close();
-
-	ofstream desktopXMLFile(desktop + aux);
-	desktopXMLFile << "<scene>\n\t<model file = �" << fileNm << "� />\n</scene>" << endl;
-}
-
+// Main function
 int main(int argc, char **argv) {
 	string operationLine, operation;
 
 	cout << "Insert your operation:" << endl;
 	getline(cin, operationLine);
-	splitted = split(operationLine, ' '); // Guarda a string num vetor com as substrings separadas por um espa�o
+	splitted = split(operationLine, ' '); // Save a string on a vector with the substrings separated by a space
 
-	// A fun��o "stoff" transforma o conte�do de uma string num float
+	// The function "stoff" transforms the content of a string in a float
 
 	if (splitted[0] == "Generator" || splitted[0] == "generator") {
 		switch (checkOP(splitted[1])) {
-			case 1:	// Plano -> recebe o comprimento e a largura, sendo os v�rtices deduzidos a partir do ponto (0, 0)
+			case 1:	// Plane -> receives the length and the width
 				if (splitted.size() == 5) {
-					filename = splitted[4] + fileExtension;
-					comprimento = stof(splitted[2]);
-					largura = stof(splitted[3]);
+					filename = splitted[4];
+					length = stof(splitted[2]);
+					width = stof(splitted[3]);
 				}
 				break;
-			case 2: // Caixa -> recebe o comprimento, a largura e a altura, sendo os v�rtices deduzidos a partir do ponto (0, 0)
+			case 2: // Box -> receives the length, the width and the height
 				if (splitted.size() == 6) {
-			        filename = splitted[5] + fileExtension;
-					comprimento = stof(splitted[2]);
-					largura = stof(splitted[3]);
-					altura = stof(splitted[4]);
+			        filename = splitted[5];
+					length = stof(splitted[2]);
+					width = stof(splitted[3]);
+					height = stof(splitted[4]);
 				}
 				break;
-			case 3:
-				if (splitted.size() == 6) { //Sphere
-			        filename = splitted[5] + fileExtension;
-                    //update Glut Functions args used in renderScene
+			case 3: // Sphere -> receives the radius, the slices and the stacks
+				if (splitted.size() == 6) { 
+			        filename = splitted[5];
 					radius = stof(splitted[2]);
 					slices = stof(splitted[3]);
 					stacks = stof(splitted[4]);
 				}
 				break;
-			case 4:
-				if (splitted.size() == 7) { //Cone
-			        filename = splitted[6] + fileExtension;
-                    filename = splitted[5] + fileExtension;
-                    //update Glut Functions args used in renderScene
-					baseRadius = stof(splitted[2]);
-					altura = stof(splitted[3]);
+			case 4: // Cone -> receives the base radius, the height, the slices and the stacks
+				if (splitted.size() == 7) { 
+			        filename = splitted[6];
+					radius = stof(splitted[2]);
+					height = stof(splitted[3]);
 					slices = stof(splitted[4]);
 					stacks = stof(splitted[5]);
 				}
@@ -413,7 +379,6 @@ int main(int argc, char **argv) {
 
 	// Create a XML file and write on it
 	create3dFile(filename);
-	writeXMLFile(filename);
 
 	return 1;
 }
