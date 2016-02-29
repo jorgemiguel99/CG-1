93c3e8357a25243e96db45034806b467166aa90f
tigas95 <tiago.carvalhais.1995@gmail.com>
2016-02-29 18:25:14 +0000

    acrescentei a leitura de xml

--- src/Fase 1/generator.cpp
+++ src/Fase 1/generator.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <fstream>
 #include <iostream>
 #include <ostream>
@@ -7,6 +8,7 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <GL/glut.h>
+#include "CG-Fase1\tinyxml\tinyxml.h"	// The place where the file "tinyxml.h" is located
 #include <math.h>
 //#include <GLUT/glut.h> //-- MAC
 
@@ -15,26 +17,29 @@
 using namespace std;
 
 // Global variables handling files
-//string desktop = "C:\\Users\\Tiago\\Desktop\\Universidade\\3º Ano\\CG\\Projeto\\Fase 1\\Ficheiros 3d";
+string desktop = "C:\\Users\\Tiago\\Desktop\\Universidade\\3º Ano\\CG\\Projeto\\Fase 1\\generatorXML.xml";
 //string desktop = "/Users/zecarlos/Desktop/";// -- MAC
 string filename;
 
 // Dimensions of the figures
 float length, width, height, radius, slices, stacks;
 
-// Variables used to move the camera
+// Variables used to move the camera 
 float moveX = 0.0f, moveY = 0.0f, moveZ = 0.0f, rotateX = 0.0f, rotateY = 0.0f, rotateZ = 0.0f, angle = 0;
 
-// Global variable process input
-vector<string> splitted;
+// Tells if the renderScene function will read from the stdin or from a .3d file
+bool read = false;
 
 // Global variable store 3d file read
 vector<string> file3dRead;
 
+// Global variable process input
+vector<string> splitted;
+
 // Operation name has 100 char at max
 #define MAX 100
 
-// Return the number to be used on the switch in main function; splitted[1] = operation name
+// Returns the number to be used on the switch in main function; splitted[1] = operation name
 int checkOP(string str) {
     if (str == "Plane" || str == "plane") return 1;
     else if (str == "Box" || str == "box") return 2;
@@ -55,108 +60,97 @@ vector<string> split(string str, char delimiter) {
     return internal;
 }
 
-//Read the vertex of figure.3d files into a vector
+// Reads the vertices of figure.3d files into a vector
 vector<string> read3d(string figure) {
 	vector<string> vecx;
 	ifstream fileTXT(figure);
 	string line;
 	while (getline(fileTXT, line)) {
-		//adding at the end of vector the line
-		vecx.push_back(line);
+		vecx.push_back(line); // Adding the line at the end of vector 
 	}
-	int size = stoi(vecx[0]); // number of total vertex-- first line in .3d file
-	// Testing the contents
-	//for (int i=0; i <= size; i++) cout << vecx[i] << endl;
-	//Returns vector with coordinates in each position
-	//vecx[1..size]
+	int size = stoi(vecx[0]); // Number of total vertices. It's the first line in the .3d file
 	return vecx;
 }
 
-void drawRenderSceneFile3d(void){
-  int i;
-  //Getting list of vertex from read3d
-  //Split vector file3dRead from read3d into vertex
-  int size = stoi(file3dRead[0]); // number of vertex
-  vector<string> vrtx; // stores all vertex read from file3dRead vector<string> stof conversion later!!
-  string line; // string processed of all file3dRead strings concatenated into one
-  int conta=0;
-  for (int i=1; i <= size; i++){
-    if(conta!=0){ //adding ',' at the end of each line except the first
-      //cout << line << endl;
-      line = line + ", "+ file3dRead[i];
-    }
-    else{
-      //cout << line << endl;
-      line += file3dRead[i];
-      conta++;
-    }
-  }
-  //cout << line << "\n\n" << endl;
-  vrtx = split(line, ',');
+// Gets the list of vertices from read3d and splits the vector file3dRead from read3d into vertices
+void drawRenderSceneFile3d(void) {
+	int i;
+	// Gets the list of vertices from read3d
+	// Splits the vector file3dRead from read3d into vertices
+	int size = stoi(file3dRead[0]); // Number of vertices
+	vector<string> vrtx; // Stores all vertex read from file3dRead vector<string> stof conversion later!!
+	string line; // String processed of all file3dRead strings concatenated into one
+	int conta = 0;
+	for (int i = 1; i <= size; i++) {
+		if (conta != 0) { // Adding ',' at the end of each line except the first
+			line = line + ", " + file3dRead[i];
+		}
+		else {
+			line += file3dRead[i];
+			conta++;
+		}
+	}
+	vrtx = split(line, ',');
 
-  //each line contains 3 float and we have size lines so we need to do size*3 in the condition
-  //Testing the list of vertex read from file 3d
-  //for(int j=1;j<size*3;j++) cout << vrtx[j] << endl;
+	// Drawing process will now take place with vrtx contents starting in vrtx[1..size*3] because vrtx[0] has the number of lines
 
-  //Drawing process will now take place with vrtx contents starting in vrtx[1..size*3] because vrtx[0] has the number of lines
+	// Clear buffers
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  // Clear buffers
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	// Set the Camera
+	glLoadIdentity();
+	gluLookAt(0.0, 0.0, 5.0, 0.0, 1.0, 0.0, 0.0f, 1.0f, 0.0f);
 
-  // Set the Camera
-  glLoadIdentity();
-  gluLookAt(0.0, 0.0, 5.0, 0.0, 1.0, 0.0, 0.0f, 1.0f, 0.0f);
+	// Geometric transformations
+	glTranslatef(moveX, moveY, moveZ);
+	glRotatef(angle, rotateX, rotateY, rotateZ);
 
-  // Geometric Transformations
-  glTranslatef(moveX, moveY, moveZ);
-  glRotatef(angle, rotateX, rotateY, rotateZ);
+	// Plane needs 6 vertices -- vrtx[1..18]
+	if (size == 6) {
+		glBegin(GL_TRIANGLES);
 
-  // Plane needs 6 vertex -- vrtx[1..18]
-  if(size==6) {
-    glBegin(GL_TRIANGLES);
+		for (i = 0; i <= 15; i += 3) {
+			glColor3f(0, 1, 0);
+			glVertex3f(stof(vrtx[i]), stof(vrtx[i + 1]), stof(vrtx[i + 2]));
+		}
 
-    for(i=0;i<=15;i+=3){
-        glColor3f(0, 1, 0);
-        glVertex3f(stof(vrtx[i]), stof(vrtx[i+1]), stof(vrtx[i+2]));
-    }
+		glEnd();
+	}
+	// Box needs 36 vertices  -- vrtx[1..108]
+	else if (size == 36) {
+		glBegin(GL_TRIANGLES);
 
-    glEnd();
-  }
-  // Box needs 36 vertex  -- vrtx[1..108]
-  else if(size==36) {
-    glBegin(GL_TRIANGLES);
+		// Lower face && Upper face
+		for (i = 0; i <= 33; i += 3) {
+			glColor3f(1, 0, 0);
+			glVertex3f(stof(vrtx[i]), stof(vrtx[i + 1]), stof(vrtx[i + 2]));
+		}
 
-    // Lower face && Upper face
-    for(i=0;i<=33;i+=3){
-        glColor3f(1, 0, 0);
-        glVertex3f(stof(vrtx[i]), stof(vrtx[i+1]), stof(vrtx[i+2]));
-    }
+		// Right face && Left face
+		for (i = 36; i <= 69; i += 3) {
+			glColor3f(0, 1, 0);
+			glVertex3f(stof(vrtx[i]), stof(vrtx[i + 1]), stof(vrtx[i + 2]));
+		}
 
-    // Right face && Left face
-    for(i=36;i<=69;i+=3){
-        glColor3f(0, 1, 0);
-        glVertex3f(stof(vrtx[i]), stof(vrtx[i+1]), stof(vrtx[i+2]));
-    }
+		// Front face && Back face
+		for (i = 72; i <= 105; i += 3) {
+			glColor3f(0, 0, 1);
+			glVertex3f(stof(vrtx[i]), stof(vrtx[i + 1]), stof(vrtx[i + 2]));
+		}
 
-    // Front face && Back face
-    for(i=72;i<=105;i+=3){
-        glColor3f(0, 0, 1);
-        glVertex3f(stof(vrtx[i]), stof(vrtx[i+1]), stof(vrtx[i+2]));
-    }
-
-    glEnd();
-  }
-  // Sphere needs 60 vertex -- CHANGE TO REAL NUMBER
-  else if(size==60) {
-      // Under construction...
-  }
-  //Cone needs 80 vertex -- CHANGE TO REAL NUMBER
-  else if(size==80) {
-  // Under construction...
-  }
+		glEnd();
+	}
+	// Sphere needs 60 vertices -- CHANGE TO REAL NUMBER
+	else if (size == 60) {
+		// Under construction...
+	}
+	//Cone needs 80 vertices -- CHANGE TO REAL NUMBER
+	else if (size == 80) {
+		// Under construction...
+	}
 }
 
-// Print the figures on the .3d files
+// Prints the figures on the .3d files
 void print3d(string figure) {
 	if (figure == "plane") {
 		ofstream plane;
@@ -401,9 +395,61 @@ void rotation(unsigned char key, int x, int y) {
 	glutPostRedisplay();
 }
 
+// Create filename.3d in desktop and in .exe folder
+void create3dFile(string fileNm) {
+	ofstream File(filename);
+	ofstream desktopFile(desktop + filename);
+}
+
+void searchXMLData(TiXmlElement* pElem, string name3d) {
+	TiXmlHandle hRoot(0);
+	TiXmlElement* pSubElem = pElem;
+	TiXmlAttribute* pAttrib = NULL;
+
+	// Set current node to root node and determine if child node exists or not  
+	hRoot = TiXmlHandle(pSubElem);
+	pSubElem = hRoot.FirstChildElement().Element();
+	if (!pSubElem) return;
+
+	char* pszText = NULL;
+
+	while (pSubElem) {
+		// Find attribute  
+		pAttrib = pSubElem->FirstAttribute();
+		while (pAttrib) {
+			char* pszText = (char*)pAttrib->Value();
+			if (pszText == name3d) {
+				printf("Found %s!\n", pszText);
+				read = true;
+				file3dRead = read3d((string) pszText); // Save the content of the .3d file in a vector
+				return;
+			}
+			pAttrib = pAttrib->Next();
+		}
+
+		// Recursive call for searching child node based on the current node  
+		searchXMLData(pSubElem, name3d);
+		pSubElem = pSubElem->NextSiblingElement();
+	}
+	printf("File not found!\n");
+}
+
+// Load the named file and dump its structure to stdout
+void readXML(const char* file, string name3d) {
+	TiXmlDocument doc;
+	doc.LoadFile(file);
+	TiXmlHandle hDoc(&doc);
+
+	// Access root node  
+	TiXmlElement* pRoot = hDoc.FirstChildElement().Element();
+	if (!pRoot) return;
+
+	// Search child node step by step from the starting root node  
+	searchXMLData(pRoot, name3d);
+}
+
 // Main function
 int main(int argc, char **argv) {
-  int read=0;
 	string operationLine, operation;
 
 	cout << "Insert your operation:" << endl;
@@ -423,23 +469,23 @@ int main(int argc, char **argv) {
 				break;
 			case 2: // Box -> receives the length, the width and the height
 				if (splitted.size() == 6) {
-			    filename = splitted[5];
+			        filename = splitted[5];
 					length = stof(splitted[2]);
 					width = stof(splitted[3]);
 					height = stof(splitted[4]);
 				}
 				break;
 			case 3: // Sphere -> receives the radius, the slices and the stacks
-				if (splitted.size() == 6) {
-			    filename = splitted[5];
+				if (splitted.size() == 6) { 
+			        filename = splitted[5];
 					radius = stof(splitted[2]);
 					slices = stof(splitted[3]);
 					stacks = stof(splitted[4]);
 				}
 				break;
 			case 4: // Cone -> receives the base radius, the height, the slices and the stacks
-				if (splitted.size() == 7) {
-			    filename = splitted[6];
+				if (splitted.size() == 7) { 
+			        filename = splitted[6];
 					radius = stof(splitted[2]);
 					height = stof(splitted[3]);
 					slices = stof(splitted[4]);
@@ -447,27 +493,15 @@ int main(int argc, char **argv) {
 				}
 				break;
 			default:
-				cout << "Not an operation of generator!\n" << endl;
+				cout << "Not an operation!\n" << endl;
 				filename = "invalid.3d";
-        //create invalid.3d
-        ofstream invalid3d(filename);
-        invalid3d << "Not an operation of generator!" << endl;
 				break;
 		}
 	}
-  else if(splitted[0] == "Read" || splitted[0] == "read"){
-    //file to be read is splitted[1] = "filename.3d";
-    file3dRead = read3d(splitted[1]);
-    //file3dRead has file contents as strings and is used in drawRenderSceneFile3d() to draw that content
-    read=1;
-  }
-	else {
-    cout << "Try again! " << endl;
-    filename = "invalid.3d";
-    //create invalid.3d
-    ofstream invalid3D(filename);
-    invalid3D << "Not an operation through all program!" << endl;
-  }
+	else if (splitted[0] == "Read" || splitted[0] == "read") {	// Read -> receives the name of the .3d file
+		if (splitted.size() == 2) readXML("generatorXML.xml", splitted[1]);
+	}
+	else cout << "Try again with this usage: generator operationName inputs filename" << endl;
 
 	// Init
     glutInit(&argc, argv);
@@ -477,13 +511,13 @@ int main(int argc, char **argv) {
     glutCreateWindow("Solar System - Stage 1");
 
     // Callback registration
-    if(read==1) {glutDisplayFunc(drawRenderSceneFile3d);} //draw the content of file read
-    else {glutDisplayFunc(renderScene);} //draw normal content
+    if(read == false) glutDisplayFunc(renderScene);
+	else glutDisplayFunc(drawRenderSceneFile3d);
     glutReshapeFunc(changeSize);
 
-	   // Registration of the keyboard
-	  glutSpecialFunc(translation);
-	  glutKeyboardFunc(rotation);
+	// Registration of the keyboard
+	glutSpecialFunc(translation);
+	glutKeyboardFunc(rotation);
 
     // OpenGL settings
     glEnable(GL_DEPTH_TEST);
